540f7873b5e91da99557cbc932b9cdd2b7552d8d
Nelson/Roberto <git@nelsonroberto.com>
2009-01-16 11:36:58 -0500

    Add Authenticated system. Allows users who are logged in to see advanced.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
   map.resources :reinventariar, :avanzado, :buscar
   map.resources :sacar, :collection => {:sacar_temporal => :put}
   map.resources :javascripts, :collection => { :nombre_de_orden => :post, :numero_de_orden => :post, :por_sacar => :post, :agregar_otro_para_sacar => :post, :cantidad_update =>:post, :factura => :post, }
