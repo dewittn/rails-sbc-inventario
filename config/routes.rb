2cf00ef9529c338285a02b1ec02001b531118106
Nelson/Roberto <git@nelsonroberto.com>
2009-02-03 14:20:37 -0500

    Cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,19 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :reinventariar, :avanzado, :buscar
+  map.resources :sacar, :collection => {:sacar_temporal => :put}
+  map.resources :javascripts, :collection => { :nombre_de_orden => :post, :numero_de_orden => :post, :por_sacar => :post, :agregar_otro_para_sacar => :post, :cantidad_update =>:post, :factura => :post, }
+  
+  map.nuevo '/nuevo', :controller => 'buscar', :action => 'new'
+  
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
   # map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
+  map.resources :users, :only => [:new,:create]
 
-  map.resource :session
+  map.resource :session, :only => [:new,:create,:destroy]
 
-  map.resources :reinventariar, :avanzado, :buscar, :marcas, :colores, :tipos, :tallas, :estilos, :generos, :settings
-  map.resources :sacar, :collection => {:sacar_temporal => :put}
-  map.resources :javascripts, :collection => { :nombre_de_orden => :post, :numero_de_orden => :post, :por_sacar => :post, :agregar_otro_para_sacar => :post, :cantidad_update =>:post, :factura => :post, }
-  map.nuevo '/nuevo', :controller => 'buscar', :action => 'new' 
-  
-  # The priority is based upon order of creation: first created -> highest priority.
+  map.resources :settings, :only => [:index]
+  map.resources :marcas, :colores, :tipos, :tallas, :estilos, :generos, :except => [:show]
 
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  #map.connect ':controller/:action/:id'
-  #map.connect ':controller/:action/:id.:format'
 end
