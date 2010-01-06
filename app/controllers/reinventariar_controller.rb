97b56bdbc8270c0b4fc8137be662facb3bbdaac8
Nelson/Roberto <git@nelsonroberto.com>
2010-01-06 10:54:16 -0500

    changes from dell. Redoing reinventario controller.

--- app/controllers/reinventariar_controller.rb
+++ app/controllers/reinventariar_controller.rb
@@ -1,22 +1,44 @@
 class ReinventariarController < ApplicationController
+  before_filter :login_required
+  
   def index
-    params[:id].blank? ? @inventarios ||= Inventario.reinventario(params[:page]) : redirect_to(edit_reinventariar_path(params[:id]))
-  end
-  
-  def edit
-    @inventarios ||= Inventario.find(id)
-  end
-  
-  def update
-    Inventario.update(id, {"cantidad" => params[:cantidad].to_i,"necesita_reinventariarse" => false})
-    @inventarios = Inventario.find(id)
+    redirect_to(avanzado_index_path) if params[:commit] == "Limpiar"
+    @sql ||= build_sql(Color,Marca,Genero,Estilo,Tipo,Talla,:id,:fila,:columna)
+    search_vars
   end
   
   def show
-  
+    @inventario ||= Inventario.find(params[:id])
   end
-    
-  def id
-    params[:id]
+  
+  def edit
+    @inventario ||= Inventario.find(params[:id])
+  end
+  
+  def update
+    @inventario ||= Inventario.find(params[:id])
+    @inventario.record_historia = true
+    @inventario.admin_changed = true
+    respond_to do |format| 
+      if @inventario.update_attributes(params[:inventario])
+        flash[:notice] = 'Chapter was successfully updated.'
+        format.html { redirect_to avanzado_path(params[:id]) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @inventario.errors.to_xml }
+      end
+    end
+  end
+
+  def create
+    @inventario = Inventario.new(params[:inventario])
+    @inventario.save ? flash[:notice] = "El registro con código <b>#{@inventario.id.to_s}</b> se creó exitosamente" : render(:action => 'new')
+  end
+  
+  def destroy
+    Inventario.update(params[:id], {"por_sacar" => 0, "tiene_por_sacar" => false, "eliminado" => true,"eliminado_at" => Time.now})
+    flash[:notice] = "El paquete ha sido marcado como eliminado"
+    redirect_to avanzado_index_path
   end
 end
