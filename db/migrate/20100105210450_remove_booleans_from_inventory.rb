85731276d999c82b59d876d0ae2776c584c311d0
Nelson/Roberto <git@nelsonroberto.com>
2010-01-06 11:53:08 -0500

    Removed unused fields form inventario.

--- db/migrate/20100105210450_remove_booleans_from_inventory.rb
+++ db/migrate/20100105210450_remove_booleans_from_inventory.rb
@@ -1,7 +1,15 @@
 class RemoveBooleansFromInventory < ActiveRecord::Migration
   def self.up
+    remove_column :inventarios, :eliminado
+    remove_column :inventarios, :eliminado_at
+    remove_column :inventarios, :necesita_reinventariarse
+    remove_column :inventarios, :tiene_por_sacar
   end
 
   def self.down
+    add_column :inventarios, :tiene_por_sacar, :boolean,          :default => false
+    add_column :inventarios, :necesita_reinventariarse, :boolean
+    add_column :inventarios, :eliminado_at, :datetime
+    add_column :inventarios, :eliminado, :boolean,                :default => false, :null => false
   end
 end
