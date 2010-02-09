9896c27ce7dc7a0daf7e809b25cdf8146afbf9df
Nelson/Roberto <git@nelsonroberto.com>
2010-02-09 13:06:17 -0500

    Increase number of packages to display.

--- app/controllers/reinventariar_controller.rb
+++ app/controllers/reinventariar_controller.rb
@@ -4,7 +4,7 @@ class ReinventariarController < ApplicationController
   def index
   	session[:row] = params[:row] unless params[:row].blank?
   	session[:column] = params[:column] unless params[:column].blank?
-    @inventarios = Inventario.pag_search( !params[:id].blank? ? read_values(:id) : {:row => session[:row], :column => session[:column]}) if params[:commit] == "Buscar"
+    @inventarios = Inventario.pag_search( !params[:id].blank? ? read_values(:id) : {:row => session[:row], :column => session[:column], :per_page => 50}) if params[:commit] == "Buscar"
   end
   
   def show
